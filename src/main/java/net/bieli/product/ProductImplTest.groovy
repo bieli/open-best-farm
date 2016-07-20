cadfde4f23fc7a5c0974d511661e3b12a5ec731e
Marcin Bielak <marcin.bieli@gmail.com>
2016-07-20 23:13:04 +0200

    add jacoo test coverage + updating tests + updating gradle.build

--- src/main/java/net/bieli/product/ProductImplTest.groovy
+++
@@ -1,9 +0,0 @@
-package net.bieli.product
-
-import spock.lang.Specification
-
-class ProductImplTest extends Specification {
-    void setup() {
-
-    }
-}
---
+++ src/test/groovy/net/bieli/product/ProductListSpec.groovy
@@ -0,0 +1,50 @@
+package net.bieli.product
+
+import net.bieli.excaptions.CapacityExceededException
+import spock.lang.Specification
+
+class ProductListSpec extends Specification {
+    def "should set and get limit in ProductList"() {
+        given:
+        def limit = 100
+        def ProductList productList = new ProductList()
+
+        when:
+        productList.setLimit(limit)
+
+        then:
+        limit == productList.getLimit()
+    }
+
+    def "should raise CapacityExceededException when limit exceed"() {
+        given:
+        def limit = 1
+        def ProductList productList = new ProductList(limit)
+
+        when:
+        productList.add(new ProductImpl(ProductKind.CORN))
+        productList.add(new ProductImpl(ProductKind.EGG))
+
+        then:
+        CapacityExceededException e = thrown()
+        e.message == "Product list is FULL ! Please, consume product first and next try ADD !"
+    }
+
+    def "should generate text report"() {
+        given:
+        def limit = 3
+        def ProductList productList = new ProductList(limit)
+
+        when:
+        productList.add(new ProductImpl(ProductKind.BUTTER))
+        productList.add(new ProductImpl(ProductKind.BUTTER))
+        productList.add(new ProductImpl(ProductKind.CORN))
+
+        then:
+        def r = productList.report()
+        r.contains("|     name |    count |")
+        r.contains("+----------+----------+")
+        r.contains("|   BUTTER |        2 |")
+        r.contains("|     CORN |        1 |")
+    }
+}
